18236eb3b951f38632e066536efc86458f5e6948
Jack Korbin Kelley <jack@iMac.local>
2017-07-06 11:00:54 +0800

    Added storyboard

---
+++ Adventure Sports/StudentCell.swift
@@ -0,0 +1,20 @@
+//
+//  StudentCell.swift
+//  Adventure Sports
+//
+//  Created by Jack Korbin Kelley on 4/7/2017.
+//  Copyright © 2017 Jack Korbin Kelley. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class  StudentCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func updateUI() {
+        
+    }
+}
---
+++ StudentCell.swift
@@ -0,0 +1,14 @@
+//
+//  StudentCell.swift
+//  
+//
+//  Created by Jack Korbin Kelley on 4/7/2017.
+//
+
+import Foundation
+
+class StudentCell : UITableViewCell {
+    
+    override func awakeFromNib()
+}
+
